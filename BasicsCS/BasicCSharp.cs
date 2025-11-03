8b112b332a9a0e52a0614b5c78add70a8969fbed
GGupta03 <gaurav.gupta26102003@gmail.com>
2025-11-03 12:02:14 +0530

    Created Basic File

---
+++ BasicsCS/BasicCSharp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzApp.BasicsCS
+{
+    class BasicCSharp
+    {
+        public BasicCSharp()
+        {
+                       Console.WriteLine("Welcome to Basic C# Programming!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BridgeLabzApp.BasicsCS;
+
+namespace BridgeLabzApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicCSharp basicCSharp = new BasicCSharp();
+        }
+
+        }
+    }
